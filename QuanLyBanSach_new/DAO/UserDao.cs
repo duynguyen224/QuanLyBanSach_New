2e01736245e3ece070e1a9ffa857851bb6a9c9ca
KienTran2000 <77290046+KienTran2000@users.noreply.github.com>
2021-05-19 15:53:58 +0700

    kienfixlogin

--- QuanLyBanSach_new/DAO/UserDao.cs
+++ QuanLyBanSach_new/DAO/UserDao.cs
@@ -16,8 +16,37 @@ namespace QuanLyBanSach_new.DAO
         {
             db = new BookShopDbContext();
         }
-
-
+        //Update by Kien
+        public int insertUser(User u)
+        {
+            db.Users.Add(u);
+            db.SaveChanges();
+            return u.ID;
+        }
+        //Update by Kien
+        public int updateUser(int id, string fullname, string username, string password, string role)
+        {
+            var u = db.Users.Where(x => x.ID == id).FirstOrDefault();
+            u.Fullname = fullname;
+            u.Username = username;
+            u.Password = password;
+            u.Role = role;
+            db.SaveChanges();
+            return u.ID;
+        }
+        //Update by Kien
+        public int deleteUser(int id)
+        {
+            var u = db.Users.Where(x => x.ID == id).FirstOrDefault();
+            db.Users.Remove(u);
+            db.SaveChanges();
+            return u.ID;
+        }
+        //Update by Kien
+        public void populateUser(DataGridView gridview)
+        {
+            gridview.DataSource = db.Users.ToList();
+        }
 
         // login theo Proc
 
