a371fbd83bc70a5b6c34346bfd7c25b90024218e
mrkoc224 <nguyenducduy224@gmail.com>
2021-05-19 15:51:29 +0700

    small fix

--- QuanLyBanSach_new/DAO/UserDao.cs
+++ QuanLyBanSach_new/DAO/UserDao.cs
@@ -17,36 +17,6 @@ namespace QuanLyBanSach_new.DAO
             db = new BookShopDbContext();
         }
 
-        public int insertUser(User u)
-        {
-            db.Users.Add(u);
-            db.SaveChanges();
-            return u.ID;
-        }
-
-        public int updateUser(int id, string fullname, string username, string password, string role)
-        {
-            var u = db.Users.Where(x => x.ID == id).FirstOrDefault();
-            u.Fullname = fullname;
-            u.Username = username;
-            u.Password = password;
-            u.Role = role;
-            db.SaveChanges();
-            return u.ID;
-        }
-
-        public int deleteUser(int id)
-        {
-            var u = db.Users.Where(x => x.ID == id).FirstOrDefault();
-            db.Users.Remove(u);
-            db.SaveChanges();
-            return u.ID;
-        }
-
-        public void populateUser(DataGridView gridview)
-        {
-            gridview.DataSource = db.Users.ToList();
-        }
 
 
         // login theo Proc
