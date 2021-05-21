e9bb75dc25518aeeb7ecebc2497b1d5c1fa80788
QuangHuyNguyen <83232436+ngquanghuy2000@users.noreply.github.com>
2021-05-21 16:30:43 +0700

    fix user buutton fuunction by Huy

--- QuanLyBanSach_new/MyUserControl/UC_User_Admin.cs
+++ QuanLyBanSach_new/MyUserControl/UC_User_Admin.cs
@@ -24,6 +24,122 @@ namespace QuanLyBanSach_new.MyUserControl
             dao.populateUser(dataGridViewUser);
 
         }
+       //fix by Huy
+       private void clearAll()
+        {
+            textBoxID.Text = "";
+            textBoxUsername.Text = "";
+            textBoxPassword.Text = "";
+            textBoxFullname.Text = "";
+            comboBoxRole.SelectedIndex = -1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            UserDao dao = new UserDao();
+            var fullname = textBoxFullname.Text;
+            var username = textBoxUsername.Text;
+            var pass = textBoxPassword.Text;
+
+            if (fullname == "" || username == "" || pass == "" )
+            {
+                MessageBox.Show("Hãy nhập đủ thông tin !"); 
+            }
+            else
+            {
+                var u = new User()
+                {
+                    Fullname = fullname,
+                    Username = username,
+                    Password = pass,
+                    Role = comboBoxRole.SelectedItem.ToString().Trim().ToLower()
+                };
+
+                var check = dao.insertUser(u);
+                if (check > 0)
+                {
+                    MessageBox.Show("Thêm thành công !");
+                }
+                dao.populateUser(dataGridViewUser);
+
+            }
+        }
+
+        private void UC_User_Admin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            clearAll();
+        }
+
+        private void dataGridViewUser_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxID.Text = dataGridViewUser.SelectedRows[0].Cells[0].Value.ToString();
+            textBoxUsername.Text = dataGridViewUser.SelectedRows[0].Cells[1].Value.ToString();
+            textBoxPassword.Text = dataGridViewUser.SelectedRows[0].Cells[2].Value.ToString();
+            textBoxFullname.Text = dataGridViewUser.SelectedRows[0].Cells[3].Value.ToString();
+            comboBoxRole.Text = dataGridViewUser.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var fullname = textBoxFullname.Text;
+            var username = textBoxUsername.Text;
+            var pass = textBoxPassword.Text;
+
+            if (fullname == "" || username == "" || pass == "")
+            {
+                MessageBox.Show("Hãy chọn 1 user để xóa !");
+            }
+            else
+            {
+                if (MessageBox.Show("Xóa User ??", "Xóa User", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    UserDao dao = new UserDao();
+                    int id = int.Parse(textBoxID.Text);
+                    var check = dao.deleteUser(id);
+                    if (check > 0)
+                    {
+                        MessageBox.Show("Xóa thành công !");
+                        dao.populateUser(dataGridViewUser);
+                        clearAll();
+                    }
+                }
+
+            }
+
+        }
+private void button1_Click(object sender, EventArgs e)
+        {
+            var fullname = textBoxFullname.Text;
+            var username = textBoxUsername.Text;
+            var pass = textBoxPassword.Text;
+
+
+            if (fullname == "" || username == "" || pass == "")
+            {
+                MessageBox.Show("Hãy chọn 1 user để sửa !");
+            }
+            else
+            {
+                UserDao dao = new UserDao();
+                var check = dao.updateUser(int.Parse(textBoxID.Text), textBoxFullname.Text, textBoxUsername.Text, textBoxPassword.Text, comboBoxRole.SelectedItem.ToString().Trim().ToLower());
+                if (check > 0)
+                {
+                    MessageBox.Show("Sửa thành công !");
+                    dao.populateUser(dataGridViewUser);
+                    clearAll();
+                }
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            clearAll();
+        }
 
 
     }
