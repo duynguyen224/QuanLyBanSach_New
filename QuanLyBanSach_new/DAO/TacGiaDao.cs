f57ef14b657859547cdd0843e737a706b0c0d23b
QuangHuyNguyen <83232436+ngquanghuy2000@users.noreply.github.com>
2021-05-21 20:44:35 +0700

    authorDAO by Huy

--- QuanLyBanSach_new/DAO/TacGiaDao.cs
+++ QuanLyBanSach_new/DAO/TacGiaDao.cs
@@ -19,7 +19,68 @@ namespace QuanLyBanSach_new.DAO
         {
             db = new BookShopDbContext();
         }
+        //fix by HHuy
+ public void populateAuthor(ComboBox c)
+        {
+            c.DataSource = db.TacGias.Select(x => x.HoTenTG).ToList();
+        }
 
+        public string getIdAuthor(string authorName)
+        {
+            var u = db.TacGias.Where(x => x.HoTenTG == authorName).FirstOrDefault();
+            if(u == null)
+            {
+                return "abc";
+            }
+            else
+            {
+                return u.ID.ToString();
+            }
+        }
+
+        public int insertAuthor(string hoten, string diachi, string tieusu, string dienthoai)
+        {
+            var h = new SqlParameter("@hoten", hoten);
+            var d = new SqlParameter("@tieusu", diachi);
+            var t = new SqlParameter("@diachi", tieusu);
+            var dt = new SqlParameter("@dienthoai", dienthoai);
+
+            int res = db.Database.ExecuteSqlCommand("exec proc_themTacGiaMoi @hoten, @tieusu, @diachi, @dienthoai ", h, d, t, dt);
+            return res;
+        }
+
+        public int updateAuthor(string hoten, string diachi, string tieusu, string dienthoai, int id)
+        {
+            var u = db.TacGias.Where(x => x.ID == id).FirstOrDefault();
+            u.HoTenTG = hoten;
+            u.DiaChi = diachi;
+            u.TieuSu = tieusu;
+            u.DienThoai = dienthoai;
+            db.SaveChanges();
+            return u.ID;
+        }
+
+        public int deleteAuthor(int id)
+        {
+            var u = db.TacGias.Where(x => x.ID == id).FirstOrDefault();
+            db.TacGias.Remove(u);
+            db.SaveChanges();
+            return u.ID;
+
+        }
+
+        public List<Author> listAuthor()
+        {
+            var res = db.Database.SqlQuery<Author>("proc_listAuthor").ToList();
+            return res;
+        }
+
+        public void updateAuthorName(int id, string newName)
+        {
+            var res = db.TacGias.Where(x => x.ID == id).FirstOrDefault();
+            res.HoTenTG = newName;
+            db.SaveChanges();
+        }
 
     }
 }
