b4bedadd183cb8fb3ff25a8e9bdc25ff780abf34
mrkoc224 <nguyenducduy224@gmail.com>
2021-05-26 16:35:58 +0700

    a

--- QuanLyBanSach_new/MyUserControl/UC_ViewSale_Admin.cs
+++ QuanLyBanSach_new/MyUserControl/UC_ViewSale_Admin.cs
@@ -18,50 +18,5 @@ namespace QuanLyBanSach_new.MyUserControl
     public partial class UC_ViewSale_Admin : UserControl
     {
         public int idDonHang;
-        public UC_ViewSale_Admin()
-        {
-            InitializeComponent();
-            populateGridInfo();
-            populateGridDetails(idDonHang);
-        }
-
-        public void populateGridInfo()
-        {
-            DonHangDao dao = new DonHangDao();
-
-            dataGridViewDonHang.DataSource = dao.getInfo();
-        }
-
-        public void populateGridDetails(int idDonHang = 1)
-        {
-            if(idDonHang == 0)
-            {
-                dataGridViewChiTietDH.DataSource = null;
-
-            }
-            else
-            {
-                ChiTietDonHangDao dao = new ChiTietDonHangDao();
-                dataGridViewChiTietDH.DataSource = dao.getDetails(idDonHang);
-
-            }
-        }
-
-        private void dataGridViewDonHang_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-            string a;
-            a = dataGridViewDonHang.SelectedRows[0].Cells[0].Value.ToString();
-            idDonHang = int.Parse(a);
-            populateGridDetails(idDonHang);
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            populateGridInfo();
-            idDonHang = 0;
-            populateGridDetails(idDonHang);
-
-        }
     }
 }
